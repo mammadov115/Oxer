5236e0a22b7a0df23e70b1fd2a969872efa1a94c
mammadov115 <mehmanmehman182@gmail.com>
2024-01-13 22:32:48 +0400

    Adding some features

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http.request import HttpRequest
 from .models import *
 from django.contrib.admin import AdminSite
 
@@ -20,7 +21,12 @@ class Slider(admin.ModelAdmin):
     ordering = ['id']
 
 
-admin.site.register(About)
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        is_empty = About.objects.count() == 0
+        return True if is_empty else False
 
 
 class ImagesInline(admin.StackedInline):
@@ -32,10 +38,35 @@ class ImagesInline(admin.StackedInline):
 class ClassesAdmin(admin.ModelAdmin):
     inlines = [ImagesInline]
 
-
+    
 admin.site.register(Blog)
-admin.site.register(FeedBackSection)
+
+
+@admin.register(FeedBackSection)
+class FeedBackSectionAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        is_empty = FeedBackSection.objects.count() == 0
+        return True if is_empty else False
+
+
 admin.site.register(FeedBack)
-admin.site.register(InfoSection)
+
+
+@admin.register(InfoSection)
+class InfoSectionAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        is_empty = InfoSection.objects.count() == 0
+        return True if is_empty else False
+
 admin.site.register(Message)
 admin.site.register(SocialMediaAccount)
+
+@admin.register(BlogsSection)
+class BlogsSectionAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        is_empty = BlogsSection.objects.count() == 0
+        return True if is_empty else False
+    
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+from collections.abc import Iterable
 from django.db import models
+import re
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -10,7 +13,7 @@ class Contact(models.Model):
 
     def __str__(self):
         return "Contact "
-
+    
 
 class Slider(models.Model):
     text = models.TextField(null=True, blank=True)
@@ -45,6 +48,16 @@ class Image(models.Model):
         return self.title
 
 
+class BlogsSection(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return "Blogs Section"
+    
+    class Meta:
+        verbose_name_plural = "Blogs Section"
+
+
 class Blog(models.Model):
     image = models.ImageField(upload_to='blog_images/%Y/%B/%d', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True, null=True, blank=True)
@@ -70,8 +83,12 @@ class FeedBack(models.Model):
     name = models.CharField(max_length=250, null=True, blank=True)
     feedback = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural= "Feedbacks"
+
     def __str__(self):
         return self.name
+    
 
 
 class InfoSection(models.Model):
@@ -79,8 +96,18 @@ class InfoSection(models.Model):
     google_map_embed_link = models.TextField(null=True, blank=True)
     footer_text = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural= "Info Section"
+
     def __str__(self):
         return "Info Section"
+    
+    def save(self,*args, **kwargs):
+        match = re.search(r'<iframe.*?src=["\'](.*?)["\']', self.google_map_embed_link)
+        if match:
+            self.google_map_embed_link = match.group(1)
+        super().save(*args, **kwargs)
+
 
 
 class Message(models.Model):
@@ -96,14 +123,15 @@ class Message(models.Model):
 class SocialMediaAccount(models.Model):
 
     class Platforms(models.TextChoices):
-        FB = "fb", "FaceBook"
+        FB = "fb", "Facebook"
         TW = "tw", "Twitter"
         IN = "in", "Linkedin"
         IG = "ig", "Instagram"
         YT = "yt", "Youtube"
 
-    platform = models.CharField(max_length=250, choices=Platforms.choices, null=True, blank=True)
+    platform = models.CharField(max_length=250, choices=Platforms.choices, null=True, blank=True, unique=True)
     link = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return self.platform
+        platform_choice = [choice.label for choice in self.Platforms if choice == self.platform]
+        return str(platform_choice[0])
